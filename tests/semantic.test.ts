3bd3840d531b110f61e30ccb56eb86f79907ca46
sailinhtut <sailinhtut76062@gmail.com>
2025-04-11 16:28:53 +0630

    Organize Folder

---
+++ tests/semantic.test.ts
@@ -0,0 +1,16 @@
+
+
+describe('Semantic Test',function(){
+	it("Return Type of parseInt",function(){
+		console.log(parseInt("null"));
+		expect(true).toBe(true);
+	})
+})
+
+
+describe('Semantic Test 2',function(){
+	it("Return Type of null",function(){
+		console.log(parseInt("null"));
+		expect(true).toBe(true);
+	})
+})
--- tests/test.ts
+++
@@ -1,7 +0,0 @@
-enum UserRole {
-	admin = 'ADMIN',
-	user = 'USER',
-	guest = 'GUEST',
-}
-
-console.log(parseInt('de3'));
